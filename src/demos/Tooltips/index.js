e8c4d427eb980c2e9d3a4855d2f9edf12455864b
Lisa Dezaki <lisadezaki@gmail.com>
2019-11-27 16:41:29 +0000

    tooltip messages added

--- src/demos/Tooltips/index.js
+++ src/demos/Tooltips/index.js
@@ -7,17 +7,20 @@ const Tooltips = ({ active, className, style }) => (
   <Section id="Tooltips" active={active}>
     <Article id="Tooltip_1" title="Tip">
       <p>
-        The main winding was of the normal <Tooltip1 tip="">lotus-o-delta</Tooltip1> type placed in <Tooltip1 tip="">panendermic
-        semi-boloid slots</Tooltip1> of the stator, every seventh conductor being connected by a <Tooltip1 tip="">non-reversible
-        tremie pipe</Tooltip1> to the <Tooltip1 tip="">differential girdle spring</Tooltip1> on the “up” end of the grammeters.
+        The main winding was of the normal <Tooltip1 tip="The type after lotus-o-gamma">lotus-o-delta</Tooltip1> type
+        placed in <Tooltip1 tip="Semi-boloid slots that are panendermic">panendermic semi-boloid slots</Tooltip1> of
+        the stator, every seventh conductor being connected by
+        a <Tooltip1 tip="A non-reversible version of the reversible tremie pipe">non-reversible tremie pipe</Tooltip1> to
+        the <Tooltip1 tip="The spring involved in the differential girdle">differential girdle spring</Tooltip1> on the “up” end of the grammeters.
       </p>
     </Article>
     <Article id="Tooltip_2" title="Definitions">
       <p>
         The turbo-encabulator has now reached a high level of development, and it’s being successfully used in the
-        operation of <Tooltip2 tip="">novertrunnions</Tooltip2>. Moreover, whenever a <Tooltip2 tip="">forescent skor
-        motion</Tooltip2> is required, it may also be employed in conjunction with a <Tooltip2 tip="">drawn reciprocation
-        dingle arm</Tooltip2>, to reduce <Tooltip2 tip="">sinusoidal repleneration</Tooltip2>.
+        operation of <Tooltip2 tip="The best kind of trunnions.">novertrunnions</Tooltip2>. Moreover, whenever
+        a <Tooltip2 tip="A standard skor motion but with additional forescence.">forescent skor motion</Tooltip2> is required,
+        it may also be employed in conjunction with a <Tooltip2 tip="A dingle arm with drawn reciprocation.">drawn reciprocation
+        dingle arm</Tooltip2>, to reduce <Tooltip2 tip="Repleneration of sinusoids">sinusoidal repleneration</Tooltip2>.
       </p>
     </Article>
   </Section>
