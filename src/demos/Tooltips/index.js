d93b83b464440c4ec45156a5693fb6d55d23e372
Lisa Dezaki <lisadezaki@gmail.com>
2019-11-27 12:44:29 +0000

    better lorem ipsum

--- src/demos/Tooltips/index.js
+++ src/demos/Tooltips/index.js
@@ -7,30 +7,17 @@ const Tooltips = ({ active, className, style }) => (
   <Section id="Tooltips" active={active}>
     <Article id="Tooltip_1" title="Tip">
       <p>
-        Volutpat consectetur{" "}
-        <Tooltip1 tip="This is a tooltip. There's extra information hidden here.">
-          suspendisse
-        </Tooltip1>{" "}
-        cubilia imperdiet ac ullamcorper mi parturient eu nullam molestie
-        faucibus ante sit congue{" "}
-        <Tooltip1 tip="This is a tooltip. There's extra information hidden here.">
-          condimentum
-        </Tooltip1>{" "}
-        nostra id.
+        The main winding was of the normal <Tooltip1 tip="">lotus-o-delta</Tooltip1> type placed in <Tooltip1 tip="">panendermic
+        semi-boloid slots</Tooltip1> of the stator, every seventh conductor being connected by a <Tooltip1 tip="">non-reversible
+        tremie pipe</Tooltip1> to the <Tooltip1 tip="">differential girdle spring</Tooltip1> on the “up” end of the grammeters.
       </p>
     </Article>
-    <Article id="Tooltip_2" title="Definition">
+    <Article id="Tooltip_2" title="Definitions">
       <p>
-        Volutpat consectetur{" "}
-        <Tooltip2 tip="This is a tooltip. There's extra information hidden here.">
-          suspendisse
-        </Tooltip2>{" "}
-        cubilia imperdiet ac ullamcorper mi parturient eu nullam molestie
-        faucibus ante sit congue{" "}
-        <Tooltip2 tip="This is a tooltip. There's extra information hidden here.">
-          condimentum
-        </Tooltip2>{" "}
-        nostra id.
+        The turbo-encabulator has now reached a high level of development, and it’s being successfully used in the
+        operation of <Tooltip2 tip="">novertrunnions</Tooltip2>. Moreover, whenever a <Tooltip2 tip="">forescent skor
+        motion</Tooltip2> is required, it may also be employed in conjunction with a <Tooltip2 tip="">drawn reciprocation
+        dingle arm</Tooltip2>, to reduce <Tooltip2 tip="">sinusoidal repleneration</Tooltip2>.
       </p>
     </Article>
   </Section>
